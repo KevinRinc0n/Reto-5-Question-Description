51d7ae3dc1c7b532adaac676a76b24329ab9f453
KevinRinc0n <rinconkdavid@gmail.com>
2023-12-04 21:54:38 -0500

    feat: :sparkles:

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿static int diferencia (int samDaily, int kellyDaily, int diferen)
+{
+    if (samDaily < 0 || kellyDaily >100 || diferen < 0 || diferen > 100){
+        return -1;
+    }
+    else{
+        int resuelveSam = diferen;
+        int resuelveKelly = 0;
+
+        int dias = 0;
+
+        while (resuelveSam >= resuelveKelly){
+            resuelveSam += samDaily;
+            resuelveKelly += kellyDaily;
+            dias++;
+        }
+        return dias;
+    }
+}
+
+Console.WriteLine(diferencia(3,5,5));
+
+/////////////////////////////////
+
+// static int secuencia (int[] array){
+//     for (int i = 0;i < array.Length;i++)
+//     {
+//         if (array[i] <0 || array[i] > Math.Pow(10,9)){
+//             return -1;
+//         }
+//     }
+//     if(array.Length < 3 || )
+//     {
+//         return -1;
+//     }
+
+//     else
+//     {
+        
+//     }
+// }
